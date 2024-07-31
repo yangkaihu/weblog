67ce5e2dfe0704fa54dff6fa9cfd29b3d7c8bc78
yangkaihu <yangkaihu@yeah.net>
2024-07-31 21:52:23 +0800

    first commit 1.1.2

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import App from '@/App.vue'
 import router   from "@/router";
 import ElementPlus from'element-plus'
 import "element-plus/dist/index.css"
+import "@/assets/css/icon/iconfont.css"
 const app= createApp(App)
     app.use(router)
     app.use(ElementPlus)
---
+++ src/views/test.vue
@@ -0,0 +1,77 @@
+<template>
+  <div></div>
+</template>
+<body >
+
+<div class="page page-1 active">
+  <div class="conten-header">
+
+    <div class="el-menu  menu" role="menubar" style="--el-menu-level: 0; width: 100%;border: none">
+      <div></div>
+      <div>weblog</div>
+
+      <div class="el-menu el-menu--horizontal menu" role="menubar"
+           style="--el-menu-level: 0; width: 90%;border: none">
+
+
+
+        <span class="el-sub-menu" role="menuitem" ariahaspopup="true" aria-expanded="false" tabindex="0"
+              style="background-color: rgba(248, 249, 250, 0.4)">
+
+        </span>
+        <span class="el-sub-menu" role="menuitem" ariahaspopup="true" aria-expanded="false"
+              tabindex="0" style="background-color: rgba(248, 249, 250, 0.4)">
+          <div class="el-sub-menu__title el-tooltip__trigger el-tooltip__trigger">
+            <i class="el-icon">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1024 1024">
+                <path fill="currentColor"
+                      d="m308.352 489.344 226.304 226.304a32 32 0 0 0 45.248 0L783.552 512A192 192 0 1 0 512 240.448L308.352 444.16a32 32 0 0 0 0 45.248zm135.744 226.304L308.352 851.392a96 96 0 0 1-135.744-135.744l135.744-135.744-45.248-45.248a96 96 0 0 1 0-135.808L466.752 195.2A256 256 0 0 1 828.8 557.248L625.152 760.96a96 96 0 0 1-135.808 0l-45.248-45.248zM398.848 670.4 353.6 625.152 217.856 760.896a32 32 0 0 0 45.248 45.248zm248.96-384.64a32 32 0 0 1 0 45.248L466.624 512a32 32 0 1 1-45.184-45.248l180.992-181.056a32 32 0 0 1 45.248 0zm90.496 90.496a32 32 0 0 1 0 45.248L557.248 602.496A32 32 0 1 1 512 557.248l180.992-180.992a32 32 0 0 1 45.312 0z"></path>
+              </svg>
+            </i> 其他 <i class="el-icon el-sub-menu__icon-arrow" style="transform: none;">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1024 1024">
+              <path fill="currentColor"
+                    d="M831.872 340.864 512 652.672 192.128 340.864a30.592 30.592 0 0 0-42.752 0 29.12 29.12 0 0 0 0 41.6L489.664 714.24a32 32 0 0 0 44.672 0l340.288-331.712a29.12 29.12 0 0 0 0-41.728 30.592 30.592 0 0 0-42.752 0z"></path>
+            </svg>
+          </i>
+          </div>
+        </span>
+        <span class="el-menu-item" role="menuitem" tabindex="0" style="background-color: rgba(248, 249, 250, 0.4)">
+          <i class="el-icon">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1024 1024">
+              <path fill="currentColor"
+                    d="M715.648 625.152 670.4 579.904l90.496-90.56c75.008-74.944 85.12-186.368 22.656-248.896-62.528-62.464-173.952-52.352-248.96 22.656L444.16 353.6l-45.248-45.248 90.496-90.496c100.032-99.968 251.968-110.08 339.456-22.656 87.488 87.488 77.312 239.424-22.656 339.456l-90.496 90.496zm-90.496 90.496-90.496 90.496C434.624 906.112 282.688 916.224 195.2 828.8c-87.488-87.488-77.312-239.424 22.656-339.456l90.496-90.496 45.248 45.248-90.496 90.56c-75.008 74.944-85.12 186.368-22.656 248.896 62.528 62.464 173.952 52.352 248.96-22.656l90.496-90.496zm0-362.048 45.248 45.248L398.848 670.4 353.6 625.152z"></path>
+            </svg>
+          </i> 友链
+        </span>
+        <span class="el-menu-item" role="menuitem" tabindex="0" style="background-color: rgba(248, 249, 250, 0.4)">
+          <i class="el-icon">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1024 1024">
+              <path fill="currentColor"
+                    d="M288 320a224 224 0 1 0 448 0 224 224 0 1 0-448 0m544 608H160a32 32 0 0 1-32-32v-96a160 160 0 0 1 160-160h448a160 160 0 0 1 160 160v96a32 32 0 0 1-32 32z"></path>
+            </svg>
+          </i> 关于
+        </span>
+      </div>
+
+      <div class="el-menu el-menu--horizontal menu" role="menubar" style="--el-menu-level: 0;border: none">
+        <span class="user-info"><span class="el-avatar el-avatar--circle el-tooltip__trigger"
+                                      style="margin-right: 3rem;">登录</span>
+        </span>
+      </div>
+    </div>
+
+  </div>
+</div>
+<!--
+  <div class="page page-2">
+
+  </div>-->
+
+</body>
+<script setup>
+
+</script>
+
+<style lang="scss">
+
+</style>
