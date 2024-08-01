1d3df156966f2bb51883ff6879be4728c55c0e2f
yangkaihu <yangkaihu@yeah.net>
2024-08-01 22:37:46 +0800

    调整了tobar

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,8 +1,13 @@
 <template>
   <div class="landing-page">
     <div class="is-background-image" role="menubar" style="--el-menu-level: 0">
+
       <div class="el-menu el-menu-horizontal" role="menubar"
            style="--el-menu-level: 0">
+        <div class="el-icon" style="width: 50px;height: 50px">
+
+        </div>
+        <div>
            <span class="el-menu-item " role="menuitem" tabindex="0">
             <i class="el-icon" style="color: rgba(126, 255, 245,1.0)">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1024 1024">
@@ -10,17 +15,22 @@
               </svg>
             </i>首页
            </span>
+        </div>
+        <div class="el-login" style="width: 50px;height: 50px">
+
+        </div>
       </div>
+
+
     </div>
 
   </div>
 
 </template>
 
-<script setup>
+<script setup lang="js">
 import {reactive, ref} from "vue"
 import "@/assets/css/login.css"
-
 
 const scrollDown = () => {
   window.scrollTo({
